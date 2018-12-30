7a1b7f213935be7fc9cc920c61b78f80d5d87c9f
Andrea Williams <anw0514@gmail.com>
2018-12-30 17:40:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,14 @@ def prime?(num)
   if num <= 0
     return false
   else
-  inc = 2
-  until inc >= num do
-    puts num % inc
-    if num % inc == 0
-      return false
-    else
-      return true
+    inc = 2
+    until inc >= num do
+      puts num % inc
+      if num % inc == 0
+        return false
+      else
+        return true
+      end
     end
     inc += 1
   end
