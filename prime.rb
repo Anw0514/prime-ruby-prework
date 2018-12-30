89025418502c2cfc5f229f746d685e60ef4cec5a
Andrea Williams <anw0514@gmail.com>
2018-12-30 17:34:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 def prime?(num)
   half = (num / 2).to_i
-  
+  inc = 2
+  while inc <= half do
+    if num % inc = 0
+      return false
+    else
+      return true
+    end
+    inc += 1
+  end
   
   
   
